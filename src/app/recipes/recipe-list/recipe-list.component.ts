49a973a07c33bbf163288a91605ce81c128ae649
tabuz <zelek.hubert@gmail.com>
2017-01-19 22:43:31 +0000

    update components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe';
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>()
+  recipe = new Recipe('Pork Stew', 'with chickpeas','http://images.bigoven.com/image/upload/t_recipe-256/garbanzo-bean-chick-pea-stew-4.jpg' );
+  secRecipe = new Recipe('Jaja', 'W sosie', 'http://static.wizaz.pl/resize/var/ezdemo_site/storage/images/kuchnia/nowosci-kulinarne/co-warto-wiedziec-o-jajkach/dlaczego-warto-jesc-jajka/103529-1-pol-PL/Dlaczego-warto-jesc-jajka.jpg?width=256&height=256')
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  }
+}
