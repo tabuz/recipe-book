227f360c1f9381bfdac70f1b1ea2cf10a5ff84a9
tabuz <zelek.hubert@gmail.com>
2017-01-24 20:38:35 +0000

    directives update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.comp
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingListAddComponent,
+    DropdownDirective,
   ],
   imports: [
     BrowserModule,
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingridients } from "../shared/ingridients";
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-
+  public items: Ingridients[] = [];
   constructor() { }
 
   ngOnInit() {
